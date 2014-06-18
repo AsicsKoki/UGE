6ad0845c4514efda3a3737135783c85f602038f4
AsicsKoki <cpt.koki@gmail.com>
2014-06-18 11:58:07 +0200

    - Bug fixes

--- app/views/monitoring/controlPanel.blade.php
+++ app/views/monitoring/controlPanel.blade.php
@@ -1,126 +1,168 @@
 @extends('layouts/main')
 @section('main')
-    <div id="wrapper">
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li><a href="{{ URL::route('controlPanel') }}">Control Panel</a>
-                </li>
-                <li><a href="{{ URL::route('measurements') }}">Current Measurements</a>
-                </li>
-                <li><a href="{{ URL::route('consumption') }}">Consumption</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <div class="widget" id="voltage"></div>
-            <div class="widget" id="power"></div>
-            <div class="widget" id="powerMax"></div>
-        </div>
+	<div id="wrapper">
+		<!-- Sidebar -->
+		<div id="sidebar-wrapper">
+			<ul class="sidebar-nav">
+				<li><a href="{{ URL::route('controlPanel') }}">Control Panel</a>
+				</li>
+				<li><a href="{{ URL::route('measurements') }}">Current Measurements</a>
+				</li>
+				<li><a href="{{ URL::route('consumption') }}">Consumption</a>
+				</li>
+			</ul>
+		</div>
+		<div class='row'>
+			<div class="widget" id="voltage"></div>
+			<div class="widget" id="power"></div>
+			<div class="widget" id="powerMax"></div>
+		</div>
 @stop
 @section('moreScripts')
 <script type="text/javascript">
-$(function () {
-    var data = {{json_encode($latestVoltage[0]['measures'][0]['vrednost'])}};
-    console.log(data);
-    $('#voltage').highcharts({
-        chart: {
-            type: 'gauge',
-            plotBackgroundColor: null,
-            plotBackgroundImage: null,
-            plotBorderWidth: 0,
-            plotShadow: false
-        },
-        title: {
-            text: 'Latest Voltage'
-        },
-        pane: {
-            startAngle: -130,
-            endAngle: +130,
-            background: [{
-                backgroundColor: {
-                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                    stops: [
-                        [0, '#FFF'],
-                        [1, '#333']
-                    ]
-                },
-                borderWidth: 0,
-                outerRadius: '109%'
-            }, {
-                backgroundColor: {
-                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                    stops: [
-                        [0, '#333'],
-                        [1, '#FFF']
-                    ]
-                },
-                borderWidth: 1,
-                outerRadius: '107%'
-            }, {
-                // default background
-            }, {
-                backgroundColor: '#DDD',
-                borderWidth: 0,
-                outerRadius: '105%',
-                innerRadius: '103%'
-            }]
-        },
-        // the value axis
-        yAxis: {
-            min: 160,
-            max: 280,
-            minorTickInterval: 'auto',
-            minorTickWidth: 1,
-            minorTickLength: 10,
-            minorTickPosition: 'inside',
-            minorTickColor: '#666',
-            tickPixelInterval: 30,
-            tickWidth: 2,
-            tickPosition: 'inside',
-            tickLength: 10,
-            tickColor: '#666',
-            labels: {
-                step: 2,
-                rotation: 'auto'
-            },
-            title: {
-                text: 'V'
-            },
-            plotBands: [{
-                from: 160,
-                to: 200,
-                color: '#DF5353' // red
-            }, {
-                from: 200,
-                to: 240,
-                color: '#55BF3B' // green
-            }, {
-                from: 240,
-                to: 280,
-                color: '#DF5353' // red
-            }]
-        },
-        series: [{
-            name: 'Speed',
-            data: [data],
-            tooltip: {
-                valueSuffix: ' V'
+	$(function () {
+
+    var gaugeOptions = {
+
+	    chart: {
+	        type: 'solidgauge'
+	    },
+
+	    title: null,
+
+	    pane: {
+	    	center: ['50%', '85%'],
+	    	size: '140%',
+	        startAngle: -90,
+	        endAngle: 90,
+            background: {
+                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
             }
-        }]
-    },
-    // Add some life
-    function (chart) {
-            // setInterval(function () {
-            //     $.AJAX({
-            //         url:"/voltageUpdate.php",
-            //         type: "GET",
-            //         success: function(data){
-            //             // data.update(newVal);
-            //         }
-            //     })
-            // }, 3000);
-    });
+	    },
+
+	    tooltip: {
+	    	enabled: false
+	    },
+
+	    // the value axis
+	    yAxis: {
+			stops: [
+				[0.1, '#55BF3B'], // green
+	        	[0.5, '#DDDF0D'], // yellow
+	        	[0.9, '#DF5353'] // red
+			],
+			lineWidth: 0,
+            minorTickInterval: null,
+            tickPixelInterval: 400,
+            tickWidth: 0,
+	        title: {
+                y: -70
+	        },
+            labels: {
+                y: 16
+            }
+	    },
+
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    y: 5,
+                    borderWidth: 0,
+                    useHTML: true
+                }
+            }
+        }
+    };
+
+    // The speed gauge
+    $('#voltage').highcharts(Highcharts.merge(gaugeOptions, {
+        yAxis: {
+	        min: 0,
+	        max: 200,
+	        title: {
+	            text: 'Speed'
+	        }
+	    },
+
+	    credits: {
+	    	enabled: false
+	    },
+
+	    series: [{
+	        name: 'Speed',
+	        data: [80],
+	        dataLabels: {
+	        	format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                   	'<span style="font-size:12px;color:silver">km/h</span></div>'
+	        },
+	        tooltip: {
+	            valueSuffix: ' km/h'
+	        }
+	    }]
+
+	}));
+
+    // The RPM gauge
+    $('#power').highcharts(Highcharts.merge(gaugeOptions, {
+        yAxis: {
+        	min: 0,
+        	max: 5,
+	        title: {
+	            text: 'RPM'
+	        }
+	    },
+
+	    series: [{
+	        name: 'RPM',
+	        data: [1],
+	        dataLabels: {
+	        	format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y:.1f}</span><br/>' +
+                   	'<span style="font-size:12px;color:silver">* 1000 / min</span></div>'
+	        },
+	        tooltip: {
+	            valueSuffix: ' revolutions/min'
+	        }
+	    }]
+
+	}));
+
+    // Bring life to the dials
+    setInterval(function () {
+    	// Speed
+        var chart = $('#container-speed').highcharts();
+        if (chart) {
+            var point = chart.series[0].points[0],
+                newVal,
+                inc = Math.round((Math.random() - 0.5) * 100);
+
+            newVal = point.y + inc;
+            if (newVal < 0 || newVal > 200) {
+                newVal = point.y - inc;
+            }
+
+            point.update(newVal);
+        }
+
+        // RPM
+        chart = $('#container-rpm').highcharts();
+        if (chart) {
+            var point = chart.series[0].points[0],
+                newVal,
+                inc = Math.random() - 0.5;
+
+            newVal = point.y + inc;
+            if (newVal < 0 || newVal > 5) {
+                newVal = point.y - inc;
+            }
+
+            point.update(newVal);
+        }
+    }, 2000);
 });
 </script>
 @stop
