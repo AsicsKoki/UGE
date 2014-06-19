aab5c67adc89f964398d320985efabe760c89d0a
AsicsKoki <cpt.koki@gmail.com>
2014-06-19 12:20:23 +0200

    - Removed the navbar - Minor edit

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -11,36 +11,8 @@
 	</head>
 	<body>
 		<div>
-			<nav class="navbar navbar-default" role="navigation">
-				<div class="container-fluid">
-				<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-					<a class="navbar-brand" href="#">United Green Energy</a>
-					</div>
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav">
-							<li><a href="{{ URL::route('homePage') }}">Home</a></li>
-							<li><a href="#">About us</a></li>
-							<li><a href="#">Contact</a></li>
-							<li><a href="{{ URL::route('monitoring') }}">Monitoring</a></li>
-						</ul>
-						<ul class="nav navbar-nav navbar-right">
-							@if (1)
-								<li><a href="{{ URL::route('analyzers') }}">Admin Panel</a>
-								</li>
-							@endif
-							<li><a href="{{ URL::route('logout') }}">Log out</a></li>
-						</ul>
-					</div><!-- /.navbar-collapse -->
-				</div><!-- /.container-fluid -->
-			</nav>
+			<img class="imgThumb" style="width:250; height:150px;" src="/img/nis.jpg"/>Benzinska stanica: Ledena Stena, Nis
+			<img class="pull-right" src="/img/analizator.png"/>
 		</div>
 		@include('partials.status')
 		{{-- SCRIPTS --}}
