d5482e6d1fd2f597be01f1c2b9872d05878d227f
misaizdaleka <milos.boskovic@gmail.com>
2014-08-17 15:12:59 +0200

    - More styling

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -1,13 +1,13 @@
 <!doctype html>
-<html class="login">
+<html>
 	<head>
 		{{-- STYLES --}}
-		{{HTML::style('css/main.css')}}
 		{{HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css')}}
 		{{HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css')}}
+		{{HTML::style('css/main.css')}}
 		@yield('moreStyles')
 	</head>
-	<body>
+	<body class="login">
 		<div class="content">
 			<div class="login-form" role="form">
 				{{ Former::open()->method('POST')->rules(array(
@@ -18,7 +18,7 @@
 				{{Former::password('password')->required()->label('password')->placeholder('Password')->class('form-control')}}
 				{{Former::hidden()->name('_token')->value(csrf_token())}}
 				<div class="control-group submit-btn">
-					{{Former::submit('Sign in')->class('form-control')}}
+					{{Former::submit('Sign in')->class('btn btn-info btn-block')}}
 				</div>
 				{{ Former::close() }}
 			</div>
@@ -27,7 +27,6 @@
 		{{-- SCRIPTS --}}
 		{{ HTML::script('https://code.jquery.com/jquery-1.11.1.min.js') }}
 		{{ HTML::script('//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js') }}
-		{{ HTML::script('https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular.min.js') }}
 		{{ HTML::script('js/main.js') }}
 		@yield('moreScripts')
 	</body>
